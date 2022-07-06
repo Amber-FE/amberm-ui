4c3b13d8a8d1cc9528da832c7d312a45cf008f4a
空色的星海 <70128222+hello7cat@users.noreply.github.com>
2022-07-06 18:06:23 +0800

    docs(docs): example

--- document/.vitepress/theme/layout/index.vue
+++ document/.vitepress/theme/layout/index.vue
@@ -6,20 +6,27 @@ const { Layout } = DefaultTheme
 const route = useRoute()
 const noFramePath = ['start', 'intro']
 const isFrameShow = ref(false)
-const data = ref()
-const baseUrl = ref('http://127.0.0.1:9909')
-const url = ref('http://127.0.0.1:9909')
+
+// const url =  process.env.RUN_ENV == 'test' ?
+// ref('http://127.0.0.1:9909') :
+// ref('https://amber-fe.github.io/amberm-ui/example/')
+const url = ref('https://amber-fe.github.io/amberm-ui/example/')
+const baseURL =  ref('https://amber-fe.github.io/amberm-ui/example/')
+// const url = ref('http://127.0.0.1:9909')
+
 watch(
   () => route.path,
   (newPath) => {
     let path = newPath.split('/')[newPath.split('/').length - 1].split('.')[0]
+
+    console.log(path)
 
     if (noFramePath.includes(path)) {
       isFrameShow.value = false
     } else {
       isFrameShow.value = true
     }
-    url.value = baseUrl.value + '/#/' + path
+    url.value = baseURL.value + '/#/' + path
   },
   { immediate: true, deep: true }
 )
