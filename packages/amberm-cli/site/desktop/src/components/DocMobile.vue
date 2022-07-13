87e3d2cb64180eb2b8e82a001b40235116d205fb
xuxins <102231395+xuxins@users.noreply.github.com>
2022-07-13 14:22:34 +0800

    style: 侧边栏样式编写

--- packages/amberm-cli/site/desktop/src/components/DocMobile.vue
+++ packages/amberm-cli/site/desktop/src/components/DocMobile.vue
@@ -17,12 +17,11 @@
     justify-content: center;
     width: 18%;
     height: 100%;
-    background-color: white;
+    background-color: white;  
     border-right: 2px solid #eeeeee;
-
     iframe {
       width: 350px;
-      height: 750px;
+      height: 900px;
       -webkit-box-shadow: #ebedf0 0 4px 12px;
       box-shadow: #ebedf0 0 4px 12px;
       border-radius: 12px;
